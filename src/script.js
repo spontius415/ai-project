6bca462f64dda2b17b55e7652d28343c585610f9
spontius415 <116679132+spontius415@users.noreply.github.com>
2023-12-07 11:43:25 -0500

    added AI generator

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-function generatePoem(event) {
+function displayPoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
@@ -7,6 +7,21 @@ function generatePoem(event) {
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "4bt8b93f064b82fc559aa03do3abc2f3";
+  let context =
+    "You are an AI assistant and a poem generator. You love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Please create a poem about this ${instructionsInput}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
